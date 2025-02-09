17e3a22bc101c4deee51b2d83db03904b38713cf
dhruvalthakor <dhruvalthakor007@gmail.com>
2025-02-09 23:23:50 +0530

    clothing-ecommerce-wabsite

--- Clothing-Ecommerce-Website-backend/index.js
+++ Clothing-Ecommerce-Website-backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 //admin
 app.use("/admin",adminrouter);
 app.use("/product",productrouter);
