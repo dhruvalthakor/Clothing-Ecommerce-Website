4a2e45b64aef544c3541a559cb5ac5aa59f8789a
dhruvalthakor <dhruvalthakor007@gmail.com>
2025-02-11 11:21:09 +0530

    clothing-ecommerce-wabsite

--- Clothing-Ecommerce-Website-backend/index.js
+++ Clothing-Ecommerce-Website-backend/index.js
@@ -11,7 +11,7 @@ const cartRouter = require("./routers/cartRouter");
 const app=express();
 
 app.use(cors({
-  origin: 'https://clothing-ecommerce-website-mu76.vercel.app/'
+  origin: 'https://clothing-ecommerce-website-mu76.vercel.app'
 }));
   
   app.use(express.json());
