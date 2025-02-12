e72c47ce0d5d976e6b4e739eb695d34f98503495
dhruvalthakor <dhruvalthakor007@gmail.com>
2025-02-12 19:32:59 +0530

    clothing-ecommerce-wabsite

--- Clothing-Ecommerce-Website-backend/index.js
+++ Clothing-Ecommerce-Website-backend/index.js
@@ -1,37 +1,39 @@
-const express=require("express");
+const express = require("express");
 const { conection } = require("./config/db");
 const adminrouter = require("./routers/adminrouter");
-const dotenv=require("dotenv").config();
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 const productrouter = require("./routers/productrouter");
 const userrouter = require("./routers/userrouter");
 const auth = require("./middleware/auth");
 const cartRouter = require("./routers/cartRouter");
 
-const app=express();
+const app = express();
 
 app.use(cors({
-  origin: 'https://clothing-ecommerce-website-mu76.vercel.app'
+  origin: 'https://clothing-ecommerce-website-seven.vercel.app/',
+  credentials: true,
+
 }));
-  
-  app.use(express.json());
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("Express Vercel"));
+
 //admin
-app.use("/admin",adminrouter);
-app.use("/product",productrouter);
- 
+app.use("/admin", adminrouter);
+app.use("/product", productrouter);
+
 //user
-app.use("/user",userrouter);
+app.use("/user", userrouter);
 app.use(auth);
-app.use("/cart",cartRouter);
+app.use("/cart", cartRouter);
 
 
-app.listen(process.env.PORT,(err)=>{
-    if (err) {
-        console.log(err);
-    }
-    conection();
-    console.log("server is started");  
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  conection();
+  console.log("server is started");
 });
