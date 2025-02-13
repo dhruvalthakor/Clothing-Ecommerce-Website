a056c914a972f28cdc81863238120508f89eda60
dhruvalthakor <dhruvalthakor007@gmail.com>
2025-02-13 14:58:04 +0530

    clothing-ecommerce-wabsite

--- Clothing-Ecommerce-Website-backend/index.js
+++ Clothing-Ecommerce-Website-backend/index.js
@@ -10,10 +10,7 @@ const cartRouter = require("./routers/cartRouter");
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
